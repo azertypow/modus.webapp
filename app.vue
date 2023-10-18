7e751a14dd8472f4a908c3658eedc615433782b9
azertypow <azertypow@gmail.com>
2023-10-18 13:20:58 +0200

    chore: init nuxt pages

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="v-app" >
+        <nuxt-page/>
+    </section>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-app {
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="v-index" >
+        coucou
+    </section>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+defineProps<{
+}>()
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-index {
+}
+</style>
