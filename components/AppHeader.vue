8536235c8af73b92083eb1940e63475764ea32cf
azertypow <azertypow@gmail.com>
2023-10-23 13:15:11 +0200

    feat: header scroll tests

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,9 +2,80 @@
     <section
         class="v-app-header"
     >
+
+        <div
+            class="v-app-header__graphic-box"
+        >
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--du"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M431.917 44V304.335C401.345 255.848 349.691 229.513 282.229 229.513C136.754 229.513 24.0222 348.616 24 514.083H211.62C211.642 447.668 259.062 398.15 321.247 398.15C384.319 398.15 431.629 447.424 431.895 513.574H431.806V700.871C431.806 855.825 538.286 957 703.802 957C868.255 957 975.776 855.825 975.776 700.871V400.45H790.246V693.489C790.246 751.458 755.457 789.41 703.802 789.41C651.083 789.41 617.342 751.458 617.342 693.489V514.083H617.452V44H431.917Z" />
+            </svg>
+
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--m"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M554.342 296.823C523.765 247.288 473.196 222 401.501 222C359.357 222 300.297 247.288 265.513 296.823V234.636H80V506.571H265.513V483.383C265.513 426.478 295.021 390.638 340.357 390.638C381.482 390.638 407.819 425.414 407.819 480.23V778.528H593.354V483.383C593.354 426.478 622.862 390.638 667.134 390.638C709.3 390.638 734.618 425.414 734.618 480.23V778.528H920.131V464.407C920.131 308.416 849.5 222 729.342 222C657.646 222 601.779 247.288 554.342 296.823Z"/>
+            </svg>
+
+
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--o"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M202 642.571H389.62C389.642 576.155 437.062 526.637 499.247 526.637C562.496 526.637 609.917 576.155 609.939 642.571H797.581C797.559 480.229 667.912 358 499.247 358C330.605 358 202.022 480.229 202 642.571Z"/>
+            </svg>
+
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--o"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M202 642.571H389.62C389.642 576.155 437.062 526.637 499.247 526.637C562.496 526.637 609.917 576.155 609.939 642.571H797.581C797.559 480.229 667.912 358 499.247 358C330.605 358 202.022 480.229 202 642.571Z"/>
+            </svg>
+
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--s"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M282.26 403.672C282.26 474.294 321.256 541.756 440.372 563.902L500.467 575.468C527.874 580.75 536.321 592.344 536.321 605.002C536.321 620.807 516.28 635.577 484.666 635.577C428.777 635.577 346.551 610.283 277 560.71V718.834C307.549 742.022 382.399 784.186 484.666 784.186C639.63 784.186 723.935 720.94 723.935 607.108C723.935 526.992 685.987 456.365 575.295 432.136L497.303 415.266C475.178 411.076 465.667 400.496 465.667 387.872C465.667 371.002 484.666 358.338 513.11 358.338C564.764 358.338 641.709 377.314 698.645 414.202V265.593C655.437 235.045 585.847 215 511.003 215C402.445 215 282.26 256.122 282.26 403.672Z"/>
+            </svg>
+        </div>
+
+        <div
+            class="v-app-header__graphic-box"
+        >
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--du"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M431.917 44V304.335C401.345 255.848 349.691 229.513 282.229 229.513C136.754 229.513 24.0222 348.616 24 514.083H211.62C211.642 447.668 259.062 398.15 321.247 398.15C384.319 398.15 431.629 447.424 431.895 513.574H431.806V700.871C431.806 855.825 538.286 957 703.802 957C868.255 957 975.776 855.825 975.776 700.871V400.45H790.246V693.489C790.246 751.458 755.457 789.41 703.802 789.41C651.083 789.41 617.342 751.458 617.342 693.489V514.083H617.452V44H431.917Z" />
+            </svg>
+
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--m"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M554.342 296.823C523.765 247.288 473.196 222 401.501 222C359.357 222 300.297 247.288 265.513 296.823V234.636H80V506.571H265.513V483.383C265.513 426.478 295.021 390.638 340.357 390.638C381.482 390.638 407.819 425.414 407.819 480.23V778.528H593.354V483.383C593.354 426.478 622.862 390.638 667.134 390.638C709.3 390.638 734.618 425.414 734.618 480.23V778.528H920.131V464.407C920.131 308.416 849.5 222 729.342 222C657.646 222 601.779 247.288 554.342 296.823Z"/>
+            </svg>
+
+
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--o"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M202 642.571H389.62C389.642 576.155 437.062 526.637 499.247 526.637C562.496 526.637 609.917 576.155 609.939 642.571H797.581C797.559 480.229 667.912 358 499.247 358C330.605 358 202.022 480.229 202 642.571Z"/>
+            </svg>
+
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--o"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M202 642.571H389.62C389.642 576.155 437.062 526.637 499.247 526.637C562.496 526.637 609.917 576.155 609.939 642.571H797.581C797.559 480.229 667.912 358 499.247 358C330.605 358 202.022 480.229 202 642.571Z"/>
+            </svg>
+
+            <svg
+                class="v-app-header__graphic-box__item v-app-header__graphic-box__item--s"
+                width="1000" height="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M282.26 403.672C282.26 474.294 321.256 541.756 440.372 563.902L500.467 575.468C527.874 580.75 536.321 592.344 536.321 605.002C536.321 620.807 516.28 635.577 484.666 635.577C428.777 635.577 346.551 610.283 277 560.71V718.834C307.549 742.022 382.399 784.186 484.666 784.186C639.63 784.186 723.935 720.94 723.935 607.108C723.935 526.992 685.987 456.365 575.295 432.136L497.303 415.266C475.178 411.076 465.667 400.496 465.667 387.872C465.667 371.002 484.666 358.338 513.11 358.338C564.764 358.338 641.709 377.314 698.645 414.202V265.593C655.437 235.045 585.847 215 511.003 215C402.445 215 282.26 256.122 282.26 403.672Z"/>
+            </svg>
+        </div>
+
         <div
             class="v-app-header__signature"
-        ><strong>modus.</strong> au service de la transition de la mobilité</div>
+        ><strong>modus.</strong> pour une mobilité durable à Genève</div>
     </section>
 </template>
 
@@ -13,8 +84,8 @@
 
 
 <script lang="ts" setup>
-defineProps<{
-}>()
+// defineProps<{
+// }>()
 
 </script>
 
@@ -31,6 +102,8 @@ defineProps<{
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-end;
+    position: relative;
+    overflow: hidden;
 }
 
 .v-app-header__signature {
@@ -38,5 +111,67 @@ defineProps<{
     background: white;
     display: flex;
     align-items: center;
+    box-sizing: border-box;
+    width: 100%;
+    padding: var(--app-gutter);
+}
+
+.v-app-header__graphic-box {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    animation: scroll 50s linear infinite;
+
+    &:nth-child(2) {
+        left: 100%;
+    }
+}
+
+@keyframes scroll {
+    0% {
+        transform: translateX(0%);
+    }
+    100% {
+        transform: translateX(-100%);
+    }
+}
+
+.v-app-header__graphic-box__item {
+    fill: #461818;
+    position: absolute;
+    width: 40vw;
+    height: 40vw;
+
+    &:nth-child(1) {
+        top: 0;
+        left: 0;
+        transform: translate(-22vw, -4vw);
+    }
+
+    &:nth-child(2) {
+        top: 0;
+        left: 0;
+        transform: translate(6.4vw, -12vw);
+    }
+
+    &:nth-child(3) {
+        top: 50%;
+        left: 0;
+        transform: translate(38vw, calc(-50% + 2vw) ) rotate(-135deg);
+    }
+
+    &:nth-child(4) {
+        top: 50%;
+        left: 0;
+        transform: translate(62vw, calc(-50% + 2vw) ) rotate(45deg);
+    }
+
+    &:nth-child(5) {
+        bottom: 0;
+        left: 0;
+        transform: translate(15.2vw, 10vw);
+    }
 }
 </style>
