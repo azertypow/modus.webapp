4231d5d290998cbeb3318b167e2f72c8576b57f2
azertypow <azertypow@gmail.com>
2023-11-17 11:25:57 +0100

    feat: add typescript dependencies and force 4.9 version to fix completion in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,21 @@ export default defineNuxtConfig({
 
     app: {
         baseURL: '/modus.webapp/',
+        head: {
+            script: [
+                {
+                    src: "https://www.youtube.com/iframe_api",
+                    async: 'false',
+                    defer: 'false',
+                }
+            ]
+        }
     },
 
     devtools: {enabled: true},
     css: [
         '@/assets/_main.scss'
-    ]
+    ],
+
+
 })
