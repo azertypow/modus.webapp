a7e614b7e99d7ac0c7573f0da094e0140a19a451
azertypow <azertypow@gmail.com>
2023-11-21 11:20:44 +0100

    feat: Deno compress args

--- compress.deno.ts
+++ compress.deno.ts
@@ -1,9 +1,17 @@
-import { exec } from "https://deno.land/x/exec/mod.ts";
+import { exec } from 'https://deno.land/x/exec/mod.ts';
 
 
-const imagePath = './public/images'
 const maxDimension = 1900
 const outputDirName = 'compress'
+const args = Deno.args
+
+if (args[0] === undefined) {
+    console.error('need path for folder image directory')
+    Deno.exit(0)
+}
+
+const imagePath = args[0]
+const compressLevel = args[1] || 5
 
 async function main() {
     try {
@@ -22,12 +30,15 @@ async function main() {
 }
 
 async function runFFMpeg(image: Deno.DirEntry) {
-    if( ! (image.isFile && image.name.endsWith(".jpg")) ) return
+    if( ! (image.isFile && (image.name.endsWith(".jpg") || image.name.endsWith(".jpeg") ) ) ) return
 
-    const ffmpegCommand = `ffmpeg -i ${imagePath}/${image.name} -vf "scale='min(${maxDimension}\\,iw):-2'" -q:v 5 ${imagePath}/${outputDirName}/${image.name}`
+    console.info('image jpg: ', image.name)
+
+    const ffmpegCommand = `ffmpeg -i ${imagePath}/${image.name} -vf "scale='min(${maxDimension}\\,iw):-2'" -q:v ${compressLevel} ${imagePath}/${outputDirName}/${image.name}`
 
     const create = await exec  ( ffmpegCommand )
-    console.log( create )
+
+    console.info(`${imagePath}/${outputDirName}/${image.name} compressed`)
 }
 
 main()
