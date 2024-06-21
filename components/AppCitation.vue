a4c2ec4898b3b495120984ce90fdaed1770dbb64
azertypow <azertypow@gmail.com>
2024-06-21 16:58:06 +0200

    fix: update style with type update

--- components/AppCitation.vue
+++ components/AppCitation.vue
@@ -26,14 +26,19 @@ const props = defineProps<{
 
 <style lang="scss" scoped >
 .v-citation {
-  width: 100%;
-  display: flex;
-  justify-content: center;
+    width: 100%;
+    display: flex;
+    justify-content: center;
 }
 
 .v-citation__text {
-  padding: 4rem 3rem 4rem;
+    padding: 4rem 3rem 4rem;
     width: 100%;
-  border-radius: var(--app-radius-small) var(--app-radius-reg) var(--app-radius-reg) var(--app-radius-large);
+    box-sizing: border-box;
+    max-width: none;
+    text-align: center;
+    color: white;
+    background: linear-gradient(to right, var(--app-color-main), var(--app-color-main--dark));
+    border-radius: var(--app-radius-small) var(--app-radius-reg) var(--app-radius-reg) var(--app-radius-large);
 }
 </style>
