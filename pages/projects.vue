2f7ca1def76ce6c2867c8776639e97f637843031
azertypow <nico@villa1203.ch>
2024-09-17 14:56:29 +0200

    feat: add all filters

--- pages/projects.vue
+++ pages/projects.vue
@@ -14,12 +14,8 @@
             <div style="display: flex; align-items: center; justify-content: center">
               <div class="v-project__filter"
                    @click="router.push({ query: {} })"
-                      v-if="activeFilterIcon">
-                <img class="v-project__filter__img"
-                     v-if="iconUrl"
-                     alt="project icon"
-                     :src="`/project_type_icons/${iconUrl}`"
-                />
+                   v-if="activeFilterIcon"
+              >
                 <div class="v-project__filter__text">
                   {{activeFilterIcon}}
                 </div>
@@ -27,6 +23,18 @@
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
                 </svg>
               </div>
+
+              <div v-else style="display: flex; gap: 1rem; flex-wrap: wrap; justify-content: center">
+                <template v-for="(value, key) of apiProjectMap">
+                  <div class="app-button app-button--small"
+                          @click="router.push({ query: {
+                      q: key
+                  } })"
+                  >{{value}}
+                  </div>
+                </template>
+              </div>
+
             </div>
           </div>
 
@@ -73,7 +81,8 @@ import {ComputedRef, defineProps, Ref, UnwrapRef} from 'vue'
 import AppPage from "~/components/AppPage.vue";
 import {LocationQueryValue} from "vue-router";
 import {
-    apiProjectMap,
+    ApiProjectMap,
+    apiProjectMap, ApiProjectType,
     IApiSingleProject, imageUrlMap, isApiProjectType
 } from "~/composable/adminApi/apiDefinitions";
 import {ApiFetchProjects} from "~/composable/adminApi/apiFetch";
