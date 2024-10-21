9c1c6313ed26aab8c49c740db84ef4595af8cb7c
azertypow <nico@villa1203.ch>
2024-10-21 17:49:44 +0200

    feat: change OG image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,13 +10,13 @@ export default defineNuxtConfig({
             meta: [
                 { property: 'og:title', content: 'modus' },
                 { property: 'og:description', content: 'modus. pour une mobilité durable à Genève' },
-                { property: 'og:image', content: 'https://modus-ge.ch/modus-webapp-footer--web--v2.jpg' },
+                { property: 'og:image', content: 'https://modus-admin.sdrvl.ch/media/pages/home/42ddbaa302-1722098276/esemble-2-resize-100.jpg' },
                 { property: 'og:url', content: 'https://modus-ge.ch/' },
                 { property: 'og:type', content: 'website' },
                 { name: 'twitter:card', content: 'summary_large_image' },
                 { name: 'twitter:title', content: 'modus' },
                 { name: 'twitter:description', content: 'modus. pour une mobilité durable à Genève' },
-                { name: 'twitter:image', content: 'https://modus-ge.ch/modus-webapp-footer--web--v2.jpg' },
+                { name: 'twitter:image', content: 'https://modus-admin.sdrvl.ch/media/pages/home/42ddbaa302-1722098276/esemble-2-resize-100.jpg' },
             ]
         },
         pageTransition: {
