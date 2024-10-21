230fd1db60c9cb59119ddcffc6526c6e3859f1fe
azertypow <nico@villa1203.ch>
2024-10-21 17:42:36 +0200

    feat: meta OG

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,16 @@ export default defineNuxtConfig({
             htmlAttrs: {
                 lang: 'fr'
             },
-            script: [
+            meta: [
+                { property: 'og:title', content: 'modus' },
+                { property: 'og:description', content: 'modus. pour une mobilité durable à Genève' },
+                { property: 'og:image', content: 'https://modus-ge.ch/modus-webapp-footer--web--v2.jpg' },
+                { property: 'og:url', content: 'https://modus-ge.ch/' },
+                { property: 'og:type', content: 'website' },
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:title', content: 'modus' },
+                { name: 'twitter:description', content: 'modus. pour une mobilité durable à Genève' },
+                { name: 'twitter:image', content: 'https://modus-ge.ch/modus-webapp-footer--web--v2.jpg' },
             ]
         },
         pageTransition: {
