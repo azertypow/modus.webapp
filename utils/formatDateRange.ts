77381b18bb59f0f7a097fce0fa8a7e1d0e3e19ce
azertypow <nico@villa1203.ch>
2024-12-15 16:34:10 +0100

    feat: date clean repetition

--- utils/formatDateRange.ts
+++ utils/formatDateRange.ts
@@ -3,15 +3,16 @@ export function formatDateRange(stringStartDate: string, stringEndDate?: string)
     const startDate = new Date(stringStartDate)
     const endDate = stringEndDate ? new Date(stringEndDate) : null
 
+    const options: Intl.DateTimeFormatOptions = { month: 'long', year: '2-digit' }
+    const formatter = new Intl.DateTimeFormat('fr-FR', options)
 
-    const options: Intl.DateTimeFormatOptions = { month: 'long', year: '2-digit' };
-    const formatter = new Intl.DateTimeFormat('fr-FR', options);
+    const start = formatter.format(startDate)
 
-    const start = formatter.format(startDate);
-    if (!endDate) {
-        return start;
-    }
+    if (!endDate) return start
 
-    const end = formatter.format(endDate);
-    return `${start} > ${end}`;
+    const end = formatter.format(endDate)
+
+    if(start === end) return start
+
+    return `${start} > ${end}`
 }
