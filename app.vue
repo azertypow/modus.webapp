03b6dffd138619841fc64d693faef7065c563fdb
azertypow <nico@villa1203.ch>
2025-04-01 07:57:32 +0200

    build

--- app.vue
+++ app.vue
@@ -16,6 +16,9 @@
                          :href="item.slug === 'home' ? '/' : `/${item.slug}`"
               >{{item.title.value}}</nuxt-link>
             </template>
+            <nuxt-link class="v-app__menu__item"
+                       href="/project/observatoire-des-mobilites/portail-de-donnees"
+            >Portail de l’observatoire des mobilités</nuxt-link>
           </div>
 
 
