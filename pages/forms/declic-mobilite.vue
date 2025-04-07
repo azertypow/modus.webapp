2b77c664f6afa5b37656ed94f784a5324f906d40
azertypow <nico@villa1203.ch>
2025-04-07 10:17:57 +0200

    feat: close form

--- pages/forms/declic-mobilite.vue
+++ pages/forms/declic-mobilite.vue
@@ -9,40 +9,8 @@
     />
     <div class="v-proto-form__bottom_content" >
       <div>
-
-        <template v-if=" ! (isChecked_moreThan18YearOld && isChecked_readPolicy) ">
-          <h2 style="margin-top: 0; text-align: center;"
-          >Pour commencer, cochez la case ci-après.</h2>
-
-          <div class="app-form__section"
-               :class="{'app-form__section--valid': isChecked_moreThan18YearOld}"
-          >
-            <input type="checkbox" v-model="isChecked_moreThan18YearOld">
-            <label class="no-counter">Je confirme avoir plus de 18 ans et souhaiter répondre au questionnaire</label>
-          </div>
-
-          <div class="app-form__section"
-               :class="{'app-form__section--valid': isChecked_readPolicy}"
-               style="margin-top: 2rem"
-          >
-            <input type="checkbox" v-model="isChecked_readPolicy">
-            <label class="no-counter">Je confirme comprendre et accepter la politique de protection des données</label>
-          </div>
-        </template>
-
-        <template v-else>
-          <h2 style="margin-top: 0; text-align: center;"
-          >C'est parti!</h2>
-        </template>
-
-      </div>
-    </div>
-
-    <div class="v-proto-form__bottom_content"
-         v-if="isChecked_moreThan18YearOld && isChecked_readPolicy"
-    >
-      <div class="v-proto-form__bottom_content__section">
-        <app-form-declic-mobility2 />
+        <h2 style="margin-top: 0; text-align: center;"
+        >Les demandes sont terminées!</h2>
       </div>
     </div>
 
@@ -116,28 +84,6 @@ const formBodyContent: IApiBody = {
             "content": {
                 "text": `
                         <h1>Demande d’inscription au "Déclic Mobilité"</h1>
-
-                        <h3>Déclic Mobilité, c’est quoi ?</h3>
-                        <p>Le concept est simple : vous stationnez votre véhicule (dans un parking prévu à cet effet) durant le mois de mai 2025 et vous recevez un accès gratuit à des alternatives de déplacement de votre choix parmi lesquelles un abonnement général CFF, un vélo/vélo électrique/vélo cargo, un abonnement Mobility, et bien d’autres services.</p>
-
-
-
-                        <h3>Conditions de participation</h3>
-
-                        <ul>
-                          <li>Avoir 18 ans ou plus</li>
-                          <li>Résider sur le territoire communal de Genève ou Carouge</li>
-                          <li>Détenir un ou plusieur(s) véhicule(s) (voiture, moto ou scooter ; thermique ou électrique)</li>
-                          <li>S’engager à ne pas utiliser son/ses véhicule(s) pendant un mois</li>
-                          <li>Participer à la séance de lancement (samedi 3 mai matin) et de restitution (lundi 2 juin soir)</li>
-                        </ul>
-                        <h6>NB : les familles ou ménages composés de plusieurs personnes peuvent participer en groupe.</h6>
-
-                        <h3>Comment sont utilisées mes données personnelles?</h3>
-
-                        <p>
-                          Les données recueillies dans le cadre de ce questionnaire seront traitées conformément à <a target="_blank" href="/notice_de_protection_des_donnees.pdf">la notice de protection des données</a>.
-                        </p>
   `
             },
             "id": "",
@@ -246,10 +192,6 @@ const formBodyContent: IApiBody = {
 
 <style>
 .v-proto-form {
-    .v-app-header__container {
-        //background: #e0d1c1;
-    }
-
     h1 {
         color: #ff4d84;
     }
