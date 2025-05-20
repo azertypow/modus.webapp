b3e958f58c82ce04abea0acbec0b6971c8b83249
azertypow <nico@villa1203.ch>
2025-05-20 22:09:07 +0200

    chore: clean

--- middleware/redirect-media.global.ts
+++ middleware/redirect-media.global.ts
@@ -1,7 +1,4 @@
 export default defineNuxtRouteMiddleware((to) => {
-
-  console.log('coucou')
-
   if (to.path.startsWith('/media/site/')) {
     const path = to.path.replace('/media/site/', '')
     return navigateTo(`https://modus-admin.sdrvl.ch/media/site/${path}`, {
