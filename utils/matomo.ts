4e6c74e8f478f8db802dd455653f2ea6013b61e0
azertypow <nico@villa1203.ch>
2025-06-25 15:18:43 +0200

    fix: is not correct instance in modus website!

--- utils/matomo.ts
+++ utils/matomo.ts
@@ -7,16 +7,16 @@ declare global {
 export function initMatomo() {
     if (typeof window !== 'undefined') {
         console.log('init matomo')
-        const _paq = (window as any)._paq = (window as any)._paq || [];
+        const _paq = window._paq = window._paq || [];
         /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
         _paq.push(['trackPageView']);
         _paq.push(['enableLinkTracking']);
         (function() {
-            var u="//matomo.modus-ge.ch/";
+            var u="//analytics.modus-ge.ch/";
             _paq.push(['setTrackerUrl', u+'matomo.php']);
             _paq.push(['setSiteId', '1']);
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            g.async=true; g.src=u+'matomo.js'; s.parentNode?.insertBefore(g,s);
         })();
     }
 }
